c0be8750ad23b2a811a9d8a28e197487009658be
Prappers <30434976+Prappers@users.noreply.github.com>
2017-07-31 12:39:39 +0200

    Update AppsFlyerPlugin.java

--- platforms/android/src/com/appsflyer/cordova/plugins/AppsFlyerPlugin.java
+++ platforms/android/src/com/appsflyer/cordova/plugins/AppsFlyerPlugin.java
@@ -318,3 +318,16 @@ public class AppsFlyerPlugin extends CordovaPlugin {
 		callbackContext.sendPluginResult(pluginResult);
 	}
 }
+document.addEventListener("deviceready", function(){
+    
+   var options = {
+             devKey:  'PyrkbYhAsYyRs8ZfmxuyPU'// your AppsFlyer devKey                
+           };
+ 
+    var userAgent = window.navigator.userAgent.toLowerCase();
+                          
+    if (/iphone|ipad|ipod/.test( userAgent )) {
+        options.appId = "123456789";            // your ios app id in app store         
+    }
+    window.plugins.appsFlyer.initSdk(options);
+}, false);
